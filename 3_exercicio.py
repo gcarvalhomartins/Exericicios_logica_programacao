ad46637f38f7ab22494ded5acdc7f05735219384
Gabriel Carvalho <gcarvalhomartins23@gmail.com>
2023-08-31 17:55:27 -0400

    nao passei do terceiro exercicio

--- 3_exercicio.py
+++ 3_exercicio.py
@@ -2,10 +2,23 @@
 # entendimento do problema, quando eu digitar a letra D, ele irá aparecer a letra C. 
 #
 # 1. passo, preciso de uma lista de todas as teclas
+lista_de_teclas = ["A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"]
 # 2. adiciono um input para o usuário digitar.
+
+#input("digite a tecla que procura ")
+
 # 3. pegar a tecla que esse usuário digitou
+
+# 3.1 coloquei uma variavel pra testar a lista
+letra = "A"
+
 # 4. verificar na lista se ela existe
+for letra in lista_de_teclas:
+    if(letra == "A"):
+        print("elemento existe")
+## por algum motivo não esta aparecendo no console
 # 5. se ela existir, aparece o antessesor dela
+
 # 6. se ela não existir, aparecer mensagem, sorry, não temos isso. 
 
 
