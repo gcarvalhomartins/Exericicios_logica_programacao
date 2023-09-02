bb118c0f237e458199e72c49ae545cf81eac6534
Gabriel Carvalho <gcarvalhomartins23@gmail.com>
2023-09-02 16:12:08 -0400

    adicionando novas respostas

--- 6_exericicio.py
+++ 6_exericicio.py
@@ -2,3 +2,16 @@
 # brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total 
 # de eleitores.
 #
+# 1. Primeiro eu tenho que ter o total dos votos 
+total_de_votos = 100
+votos_brancos = 20
+votos_nulos = 25
+
+# 2. Agora eu preciso calcular a porcentagem que equivale de cada tipo de voto.
+calculo_total_de_votos = total_de_votos + votos_brancos + votos_nulos
+porcentagem_votos_brancos = votos_brancos * 100 / total_de_votos
+porcentagem_votos_nulos = votos_nulos * 100 / total_de_votos
+
+print(f"tivemos o total de: {calculo_total_de_votos} votos")
+print(f"onde:{porcentagem_votos_brancos:.2f}% foram de votos em branco")
+print(f"onde:{porcentagem_votos_nulos:.2f}% foram de votos Nulos")
---
+++ 7_exericicio.py
@@ -0,0 +1,13 @@
+# 7) Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste. 
+# Calcular e escrever o valor do novo salário.
+#
+#primeiro eu tenho que ter o salário
+salario_mensal = 1350
+
+#agora eu preciso da porcentagem de reajuste 
+reajuste = 1.3
+
+# calculo de reajuste
+salario_atual = salario_mensal * reajuste
+
+print(f"o salario atual e: R${salario_atual} com o reajuste de {reajuste}%")
